1fcd4624b52a4f46fc41e33e77a0ca0da520b452
Hyunsoo Park <rex8312@gmail.com>
2015-03-19 14:11:08 +0900

    출력 데이터 저장

--- C2Blocks/plot3.py
+++ C2Blocks/plot3.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     db = shelve.open('results.pkl')
     for key in db.keys():
         puzzle = db[key]
-        pl.imshow(puzzle, interpolation='none', cmap='hsv')
-        pl.savefig('data2/{}.png'.format(key))
+        #pl.imshow(puzzle, interpolation='none', cmap='hsv')
+        pl.imshow(puzzle, interpolation='none', cmap='gray')
+        pl.savefig('data3/{}.png'.format(key))
     db.close()
