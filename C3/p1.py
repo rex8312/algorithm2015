fa426167868f05f630a1307f5fa69e3ebcbafdd8
Hyunsoo Park <rex8312@gmail.com>
2015-03-19 23:41:43 +0900

    1번 문제 시각화 완료

--- C3/p1.py
+++ C3/p1.py
@@ -10,10 +10,6 @@ W = 1
 B = 0
 
 
-def draw(tree):
-    pass
-
-
 def decode(code):
     # 코드를 입력받아 트리형태로 출력함
     root = list()
@@ -38,13 +34,16 @@ def decode(code):
     return root
 
 
-def encode(tree):
-    def is_leaf(node):
-        for e in node:
-            if isinstance(e, list):
-                return False
+def is_leaf(node):
+    if isinstance(node, int):
         return True
 
+    for e in node:
+        if isinstance(e, list):
+            return False
+    return True
+
+def encode(tree):
     def flat(node):
         if is_leaf(node):
             return node[:]
@@ -61,12 +60,6 @@ def encode(tree):
 
 
 def mirror(tree):
-    def is_leaf(node):
-        for e in node:
-            if isinstance(e, list):
-                return False
-        return True
-
     def swap(node):
         if is_leaf(node):
             node[0], node[1], node[2], node[3] = node[2], node[3], node[0], node[1]
@@ -92,12 +85,49 @@ def print_code(code):
     print
 
 
+def draw(tree, title='none'):
+    def get_depth(node):
+        if is_leaf(node):
+            return 1
+        else:
+            return 1 + max([get_depth(child) for child in node])
+
+    def get_color(x, y, tree, r):
+        idx = 0
+        _x, _y = x, y
+        if x >= r/2:
+            idx += 1
+            _x -= r/2
+
+        if y >= r/2:
+            idx += 2
+            _y -= r/2
+
+        if not isinstance(tree[idx], list):
+            return tree[idx]
+        else:
+            return get_color(_x, _y, tree[idx], r / 2)
+
+    depth = get_depth(tree)
+    img = [[0.5] * 2 ** depth for _ in range(2 ** depth)]
+
+    for y in range(2 ** depth):
+        for x in range(2 ** depth):
+            img[y][x] = get_color(x, y, tree, 2 ** depth)
+
+    import pylab as pl
+    pl.imshow(img, interpolation='None', cmap='gray')
+    pl.savefig('{}.png'.format(title))
+
+
 if __name__ == '__main__':
     code = [X, W, W, W, B, X, W, X, W, B, B, B, W, W, X, X, X, W, W, B, B, B, W, W, W, W, B, B]
     tree = decode(code)
+    draw(tree, 'original')
     tree = mirror(tree)
     icode = encode(tree)
 
     print_code(code)
     print_code(icode)
+    draw(tree, 'inverted')
 
