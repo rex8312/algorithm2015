146e95bdbe8626da3ba04f8ca861226aa10fb3f7
Hyunsoo Park <rex8312@gmail.com>
2015-04-28 15:50:19 +0900

    C6

--- C6/p2-1.py
+++ C6/p2-1.py
@@ -8,16 +8,45 @@ def test_cases():
 
 
 def bf_search(n, xs):
-    for x1 in xs:
-        for x2 in xs:
-            if x1 != x2:
-                for i in range(len(x1)):
-                    pre_x1, post_x1 = x1[:i], x1[i:]
-                    if x2.startswith(post_x1):
-                        print pre_x1 + x2, '=', x1, '+', x2
+    def find_unused_number(n, xs):
+        ys = set(range(n)) - set(xs)
+        return list(ys)
+
+    def gen_seq(n, xs=list()):
+        if len(find_unused_number(n, xs)) == 0:
+            yield xs
+        else:
+            for x in find_unused_number(n, xs):
+                for xs_ in gen_seq(n, xs[:] + [x]):
+                    yield xs_
+
+    strings = list()
+    for orders in gen_seq(n):
+        tx = [None for _ in range(len(orders))]
+        for ord, x in zip(orders, xs):
+            tx[ord] = x
+
+        buff = ''
+        for i, x in enumerate(tx):
+            possible = list()
+            for j in range(1, len(x)):
+                if buff.endswith(x[:j]):
+                    possible.append((j, x[:j]))
+
+            if len(possible) == 0:
+                buff += x
+            else:
+                possible = sorted(possible, key=lambda item: len(item[1]))
+                j = possible[0][0]
+                buff += x[j:]
+
+        strings.append(buff)
+
+    strings.sort(key=lambda x: len(x))
+    return filter(lambda x: len(x) == len(strings[0]), strings)
 
 
 if __name__ == '__main__':
 
     for n, xs in test_cases():
-        bf_search(n, xs)
+        print bf_search(n, xs)
